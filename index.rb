2f35f0ec8fb407c850688a4e4e1ec3fc9f59db2a
LuisaZ <luisaziccarelli@gmail.com>
2019-11-05 09:46:15 +1100

    initial commit

--- index.rb
+++ index.rb
@@ -1,10 +1,24 @@
-
-my_hash = {
-    "birthday" => "6th june" ,
-    "height" => "172 cm" , 
-    "place of birth"  => "Westmead" , 
-    "status" => "educator"
+ 
+people = [
+    [
+    {
+    :name => "bianca",
+    "birthday" => "6th june", 
+    "height" => "172 cm", 
+    "place of birth"  => "Westmead", 
+    "status" => "educator1"
+    },
+    {
+    :name => "alex",
+    "birthday"  => "17th march", 
+    "height" => "173 cm", 
+    "place of birth"  => "Adelaide", 
+    "status" => "educator2"
     }
+]
+]
+#puts people [0][0]["status"]
+#puts people[0][0][:name]
 
-
-puts 
+puts people[0][0].key("bianca").object_id
+puts people[0][1].key("alex").object_id

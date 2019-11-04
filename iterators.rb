5a58cb817509870f7ebd7486a3e6e6a7e890a634
LuisaZ <luisaziccarelli@gmail.com>
2019-11-05 09:46:29 +1100

    initial commit

---
+++ iterators.rb
@@ -0,0 +1,9 @@
+
+
+capital_cities = { :nsw => "Sydney", :vic => "Melbourne", :qld => "Brisbane" } 
+
+capital_cities.each do |key, value|
+    p "the key is #{key}"
+    p "the value is #{value}"
+end 
+

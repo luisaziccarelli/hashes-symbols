08be053f608867800421b5382cd871b4826eaa2f
LuisaZ <luisaziccarelli@gmail.com>
2019-11-05 09:46:36 +1100

    initial commit

---
+++ seasonschedule.rb
@@ -0,0 +1,22 @@
+# season schedule
+
+
+seasons_of_year = [
+    {
+    "spring" => 244-305,
+    "summer" => 335..365,
+    "autumm" => 60-121,
+    "winter" => 152-213,
+    },
+{
+    "morning" => 3-11,
+    "noon" => 12-2,
+    "afternoon" => 3-6,
+    "evening" => 7-9,
+    "nighttime" => 10-11,
+    "midnight" => 12-3
+}
+]
+
+
+puts "Today is a
